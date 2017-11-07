99b9f352a48f0b8a795adb13e6eb282fdff273c2
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2017-11-07 14:33:29 -0500

    alpha archive

--- createDeploy.ps1
+++ createDeploy.ps1
@@ -1,8 +1,8 @@
 $date = (Get-Date).ToString("yyyy-MM-dd HH-mm")
 $src  = "C:\Users\kevin_000\Documents\DevWorkSpaces\storedd\"
 $outfile = "C:\Users\kevin_000\Documents\DevWorkSpaces\deploy\deploy_v2-" + $date + ".zip"
-$ex = @("lib","install",".vscode")
-Remove-Item -path "C:\Users\kevin_000\Documents\DevWorkSpaces\deploy\*" -Filter *deploy*.zip -whatif
+$ex = @("lib","install",".vscode","vendor/squizlabs")
+
 Get-ChildItem $src | where {$_.Name -notin $ex} | Compress-Archive -DestinationPath $outfile -Update -CompressionLevel Fastest 
 
 
