64238ad7fbaa94bcf3c1b9cd2bb7d35118895d2d
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2017-11-09 14:02:39 -0500

    deploy scripts improved

--- powershell/createDeploy.ps1
+++ powershell/createDeploy.ps1
@@ -1,6 +1,6 @@
 $date = (Get-Date).ToString("yyyy-MM-dd HH-mm")
 $src  = "C:\Users\kevin_000\Documents\DevWorkSpaces\storedd\"
 $outfile = "C:\Users\kevin_000\Documents\DevWorkSpaces\deploy\deploy_v2-" + $date + ".zip"
-$ex = @("lib","install",".vscode","createDeploy.ps1","index.1.html","node_modules",)
+$ex = @("lib","install",".vscode","createDeploy.ps1","index.1.html","node_modules","powershell","index.html")
 
 Get-ChildItem $src | where {$_.Name -notin $ex} | Compress-Archive -DestinationPath $outfile -Update -CompressionLevel Fastest 
