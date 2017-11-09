9a6957d49c665ca8dea3cbb343e891c9e0cf0bc1
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2017-11-09 18:35:49 -0500

    deploy scripts

--- powershell/createDeploy.ps1
+++ powershell/createDeploy.ps1
@@ -3,4 +3,4 @@ $src  = "C:\Users\kevin_000\Documents\DevWorkSpaces\storedd\"
 $outfile = "C:\Users\kevin_000\Documents\DevWorkSpaces\deploy\deploy_v2-" + $date + ".zip"
 $ex = @("lib","install",".vscode","createDeploy.ps1","index.1.html","node_modules","powershell","index.html")
 
-Get-ChildItem $src | where {$_.Name -notin $ex} | Compress-Archive -DestinationPath $outfile -Update -CompressionLevel Fastest 
+Get-ChildItem $src | Where-Object {$_.Name -notin $ex} | Compress-Archive -DestinationPath $outfile -Update -CompressionLevel Fastest 
