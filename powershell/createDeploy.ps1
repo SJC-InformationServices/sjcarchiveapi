5a2d00446ed15a18855253a9eccfaec4f790da15
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-01-05 12:07:20 -0500

    new deploy scripts

--- powershell/createDeploy.ps1
+++ powershell/createDeploy.ps1
@@ -1,20 +1,2 @@
-$date = (Get-Date).ToString("yyyy-MM-dd HH-mm")
-$src  = "C:\Users\kevin_000\Documents\DevWorkSpaces\storedd"
-$outfile = "C:\Users\kevin_000\Documents\DevWorkSpaces\deploy\archive\"
-$deployfile = "C:\Users\kevin_000\Documents\DevWorkSpaces\deploy\archive-" + $date + ".zip"
-Get-ChildItem $src -Recurse | 
-Where-Object {$_.FullName -like "*.php"} | 
-ForEach-Object{
-    $PathArray = $_.FullName.Replace($src,"").ToString().Split('\') 
-    $Folder = $outfile
-    for ($i=1; $i -lt $PathArray.length-1; $i++) {
-        $Folder += "\" + $PathArray[$i]
-        if (!(Test-Path $Folder)) {
-            New-Item -ItemType directory -Path $Folder
-        }
-    }   
-    $NewPath = Join-Path $outfile $_.FullName.Replace($src,"")
-    Copy-Item -Path $_.FullName -Destination $NewPath -Force
-}
-Add-Type -assembly "system.io.compression.filesystem";
-[io.compression.zipfile]::CreateFromDirectory('C:\Users\kevin_000\Documents\DevWorkSpaces\deploy\archive\', $deployfile);
+Import-Module AWSPowerShell
+git archive -v -o archive.zip --format=zip
