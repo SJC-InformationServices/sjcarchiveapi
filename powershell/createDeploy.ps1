6887b3e6ad116596f29101f931a9954411ff8cbf
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-01-05 12:52:45 -0500

    gitignore and deploy

--- powershell/createDeploy.ps1
+++ powershell/createDeploy.ps1
@@ -1,2 +1,6 @@
 Import-Module AWSPowerShell
-git archive -v -o archive.zip --format=zip
+$date = get-date -format yyyy-MM-dd-THHmmss
+$datestr = $date.ToString()
+$file = "deploy\archive-"+$datestr+".zip"
+$cmd = git archive -v -o $file --format=zip HEAD
+#Write-S3Object -BucketName elasticbeanstalk-us-east-1-813784960196 -File $file
