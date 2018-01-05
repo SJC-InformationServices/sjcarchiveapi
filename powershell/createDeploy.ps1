54fb97d304d1138a85f65ec5ef4201dc9effb92f
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-01-05 16:50:44 -0500

    new deployment scripts

--- powershell/createDeploy.ps1
+++ powershell/createDeploy.ps1
@@ -1,6 +1,10 @@
 Import-Module AWSPowerShell
-$date = get-date -format yyyy-MM-dd-THHmmss
+Set-DefaultAWSRegion -Region us-east-1
+
+$date = get-date -format yyyy-MM-dd-HHmmss
 $datestr = $date.ToString()
 $file = "deploy\archive-"+$datestr+".zip"
 $cmd = git archive -v -o $file --format=zip HEAD
-#Write-S3Object -BucketName elasticbeanstalk-us-east-1-813784960196 -File $file
+Write-S3Object -BucketName sjcarchivefiles-dev -File $file
+New-EBApplicationVersion -ApplicationName SJC_Archive -VersionLabel $file -SourceBuildInformation_SourceType Zip -SourceBuildInformation_SourceRepository S3 -SourceBuildInformation_SourceLocation sjcarchivefiles-dev/$file -Process 0
+#Update-EbApplicationVersion -ApplicationName SJC_Archive -VersionLabel $file
