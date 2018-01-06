0c9ae5ec0161c0e3c8c46b49c03fde457117b006
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-01-05 19:38:58 -0500

    update manager

--- src/storedd/modules/manager.php
+++ src/storedd/modules/manager.php
@@ -6,31 +6,36 @@ class entdef extends \RedBean_SimpleModel
     public function open() {
         global $lifeCycle;
         $lifeCycle .= "called open: ".$this->id;
+        echo $lifeCycle;
      }
      public function dispense() {
          global $lifeCycle;
          $lifeCycle .= "called dispense() ".$this->bean;
+         echo $lifeCycle;
      }
      public function update() {
          global $lifeCycle;
          $lifeCycle .= "called update() ".$this->bean;
+         echo $lifeCycle;
      }
      public function after_update() {
          global $lifeCycle;
          $lifeCycle .= "called after_update() ".$this->bean;
+         echo $lifeCycle;
      }
      public function delete() {
          global $lifeCycle;
          $lifeCycle .= "called delete() ".$this->bean;
+         echo $lifeCycle;
      }
      public function after_delete() {
          global $lifeCycle;
          $lifeCycle .= "called after_delete() ".$this->bean;
+         echo $lifeCycle;
      }
-
 }}
 namespace storedd\modules{
-include_once "../storedd/models/entdef.php";
+
 class manager extends base_api
 {
     public function __construct($request) {
