181727ce327f6c62eff5943519422a6e498fb3f5
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-01-08 15:38:35 -0500

    dev deploy

--- src/storedd/modules/manager.php
+++ src/storedd/modules/manager.php
@@ -44,10 +44,12 @@ class manager extends base_api
             }
             
             $r = \R::exportAll($b,TRUE);
-            /*foreach($r as $k=>$v){
-                $r[$k]['children'] = array_column(\R::getAll('select `name` from `entdef`,`entdef_entdef` where `entdef`.`id` = `entdef_entdef`.`child_entdef` and `entdef_entdef`.`parent_entdef` = :pid ',[':pid'=>$r[$k]['id']]),'name');
-                $r[$k]['parents'] = array_column(\R::getAll('select `name` from `entdef`,`entdef_entdef` where `entdef`.`id` = `entdef_entdef`.`parent_entdef` and `entdef_entdef`.`child_entdef` = :pid ',[':pid'=>$r[$k]['id']]),'name');
-            }*/
+            foreach($r as $k=>$v){
+                $r[$k]['children'] = $r[$k]["ownEntdef_entdef"];
+                unset($r[$k]["ownEntdef_entdef"]);
+                /*$r[$k]['children'] = array_column(\R::getAll('select `name` from `entdef`,`entdef_entdef` where `entdef`.`id` = `entdef_entdef`.`child_entdef` and `entdef_entdef`.`parent_entdef` = :pid ',[':pid'=>$r[$k]['id']]),'name');
+                $r[$k]['parents'] = array_column(\R::getAll('select `name` from `entdef`,`entdef_entdef` where `entdef`.`id` = `entdef_entdef`.`parent_entdef` and `entdef_entdef`.`child_entdef` = :pid ',[':pid'=>$r[$k]['id']]),'name');*/
+            }
             return $r;
 			break;
             case 'POST':
