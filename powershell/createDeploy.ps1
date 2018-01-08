f1c16ba56a3324be1d3d19cd10effc0f60d27915
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-01-08 12:35:40 -0500

    new deploy

--- powershell/createDeploy.ps1
+++ powershell/createDeploy.ps1
@@ -5,7 +5,8 @@ $date = get-date -format yyyy-MM-dd-HHmmss
 $datestr = $date.ToString()
 $name = "archive-"+$datestr+".zip"
 $file = "deploy\"+$name
-git add --all && git commit -m "comment"
+git add --all 
+git commit -m "dev deploy-"+$name
 git archive -v -o $file --format=zip HEAD
 Write-S3Object -BucketName sjcarchivefiles-dev -File $file
 aws elasticbeanstalk create-application-version --application-name "SJC_Archive" --version-label $name --description sjc_archive_dev --source-bundle S3Bucket="sjcarchivefiles-dev",S3Key=$name
