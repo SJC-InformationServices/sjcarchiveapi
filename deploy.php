e20b12edb2f2d223cb4ee0c3fb61bf678e09c7f4
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-01-08 18:49:10 -0500

    dev deploy

--- deploy.php
+++ deploy.php
@@ -3,7 +3,7 @@ list($enta,$entb,$entc,$entd,$ente) = \R::dispense('entdef',5);
 
 $attribdef = \R::dispense('attribdef');
 $attribdef->name ="name";
-$attribdef->label = strtoupper($entdef->name);
+$attribdef->label = strtoupper($attribdef->name);
 $attribdef->type = "text";
 $attribdef->defaultvalue = "NULL";
 $attribdef->options = array();
@@ -13,7 +13,7 @@ $attribdef->updatedon = date("Y-m-d H:i:s");
 $entties = [$enta,$entb,$entc,$entd,$ente];
 foreach($entties as $e){
     $e->name = "seasons";
-    $e->label = strtoupper($en->name);
+    $e->label = strtoupper($e->name);
     $e->createdon = date("Y-m-d H:i:s");
     $e->updatedon = date("Y-m-d H:i:s");
     $e->sharedAttribdefList[]  = $attribdef;
