ea5a49b754bf361416b712f3928a27fda2c44661
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-02-14 12:06:03 -0500

    folder and deploy structure changed

--- powershell/uploadS3.ps1
+++
@@ -1,3 +0,0 @@
-aws s3 cp "c:\Users\kevin_000\Documents\DevWorkSpaces\storedd\index.html" s3://sjcarchivefiles-dev
-aws s3 cp "c:\Users\kevin_000\Documents\DevWorkSpaces\storedd\lib\archive\css\archive.css" s3://sjcarchivefiles-dev/lib/archive/css/
-aws s3 cp "c:\Users\kevin_000\Documents\DevWorkSpaces\storedd\lib\archive\js\" s3://sjcarchivefiles-dev/lib/archive/js/ --recursive
